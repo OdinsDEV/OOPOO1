2ec9c91acb9ae1c16c6323ff2e76f38d9a797a94
Niks Ričards Odiņš <niks.odins@ventspils.edu.lv>
2025-01-16 09:29:57 +0200

    Pievienoju ABC un govs class

--- galvenais.py
+++ galvenais.py
@@ -1,26 +1,37 @@
-class dzivnieks():
+from abc import ABC, abstractmethod
+
+class dzivnieks(ABC): #Parent class
     def __init__(self,vards,kajas):
         self.vards=vards
         self.kajas=kajas
+
+    @abstractmethod        
     def skanja(self):
-        #print("Es esmu ", self.vards, "un man ir", self.kajas, "kājas.")
         print("Random animal noise")
     def __str__(self):
         return f"{self.vards} un {self.kajas} kajas"
 
-class suns(dzivnieks):
+class suns(dzivnieks): #Child class
     def __init__(self, vards, kajas):
         super().__init__(vards,kajas)
         self.vards="Komisars "+self.vards
+    def skanja(self):
+        print("Wof wof!")
 
-class kakis(dzivnieks):
+class kakis(dzivnieks): #Child class
     def __init__(self, vards, kajas):
         super().__init__(vards,kajas)
         self.vards="Kaķītis "+self.vards
+    def skanja(self):
+        print("Mjau Mjau!")
 
-listDzivnieki = []        
+class govs(dzivnieks):
+    def skanja(self):
+        print("Muuuuuuuu!")
 
-listDzivnieki.append(suns("volvis", 4))
+listDzivnieki = []       #izveidots dzivnieku saraksts 
+
+listDzivnieki.append(suns("volvis", 4)) #sarakstam pievieno dzivnieku
 listDzivnieki.append(suns("Bella", 4))
 listDzivnieki.append(suns("Dalmacietis", 4))
 
@@ -28,10 +39,12 @@ listDzivnieki.append(kakis("Pichuks", 4))
 listDzivnieki.append(kakis("Rozine", 4))
 listDzivnieki.append(kakis("Rizhais", 4))
 
-for dzivnieks in listDzivnieki:
+listDzivnieki.append(govs("Gauja", 4))
+
+
+for dzivnieks in listDzivnieki: #Izprintē katru dzīvnieku 
     print(dzivnieks)
     dzivnieks.skanja()
-
 
 #d1=dzivnieks("Gauja", 4)
 # d1.skanja()
