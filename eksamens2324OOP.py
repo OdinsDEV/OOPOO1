0d5d0a445a63b077dadb44a3e108fbea5e27b018
Niks Ričards Odiņš <niks.odins@ventspils.edu.lv>
2025-01-16 10:34:51 +0200

    1Uzdevums pabeigts

--- eksamens2324OOP.py
+++ eksamens2324OOP.py
@@ -1,3 +1,16 @@
-
 class doktorats():
-    def __init__(self, nosaukums, skaits)
+    def __init__(self, nosaukums="N/A", skaits=0):
+        self.skaits = skaits
+    def ievade(self):
+        self.nosaukums= input("Ievadiet nosaukumu: ")
+        self.skaits = int(input("Ievadiet pacientu skaitu: "))
+
+    def izvade(self):
+        galotne=""
+        if (self.skaits%10 !=1):
+            galotne="s"
+        print(f"Doktorāts {self.nosaukums} apkalpo {self.skaits} pacientu{galotne}.")    
+
+d1= doktorats("slimnica", 21)
+d1.ievade()
+d1.izvade()
