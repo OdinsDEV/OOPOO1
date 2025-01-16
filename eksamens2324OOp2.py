36641f58ebfbe2c8be18528edd5372f9db80c5fc
Niks Ričards Odiņš <niks.odins@ventspils.edu.lv>
2025-01-16 11:01:50 +0200

    viduscels

--- eksamens2324OOp2.py
+++ eksamens2324OOp2.py
@@ -1,14 +1,24 @@
 class Skolotajs():
-    def __init__(self,hSkaitsNed, tips):
-        self.hSkaitsNed = hSkaitsNed
-
+    StunduSkaitsNed =0
+    skolotajaTips= 0
+    uzvards="Nezinams"
 class SakumskolasSkolotajs(Skolotajs):
-
-
-
-
+    klase="x.i"
+    def __init__(self):
+        self.tips =1
     def izvade(self):
-        print(f"")
+        print(f"sākumskolas (tips- {self.tips}) skolotājs {self.uzvards} māca {skaits} stundas {klase} klasē")
 
 class VidusskolasSkolotajs(Skolotajs):
-    def izvade():        
+    def __init__(self):
+        self.tips =3
+    pirmaisPrieksmets= "x priekšmets"
+    otraisPrieksmets= "y priekšmets"
+    pirmaisPrieksmetsStundas= 0
+    otraisPrieksmetsStundas= 0
+    def cikStundasKopa(self):
+        self.stunduskaitsned= self.pirmaisPrieksmetsStundas + self.otraisPrieksmetsStundas
+    def izvade():    
+        print(f"abc")
+
+    
