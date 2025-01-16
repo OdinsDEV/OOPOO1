801e4fcf6cd86630c30661af73c84f4827a75c54
Niks Ričards Odiņš <niks.odins@ventspils.edu.lv>
2025-01-16 10:47:43 +0200

    Sākums

---
+++ eksamens2324OOp2.py
@@ -0,0 +1,14 @@
+class Skolotajs():
+    def __init__(self,hSkaitsNed, tips):
+        self.hSkaitsNed = hSkaitsNed
+
+class SakumskolasSkolotajs(Skolotajs):
+
+
+
+
+    def izvade(self):
+        print(f"")
+
+class VidusskolasSkolotajs(Skolotajs):
+    def izvade():        
